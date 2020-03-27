a7b09655c277e49936e2de8bbe6d0c045be45645
Tamaya Newborn <tamayanewborn@Tamayas-MacBook-Air.local>
2020-03-27 17:51:33 -0400

    json

--- csce242/inclass/mar24/script.js
+++ csce242/inclass/mar24/script.js
@@ -1,9 +1,38 @@
-function showShoes(){
+async function showShoes(){
+    let response = await fetch('https://tamayanewborn.github.io/csce242/json/shoes.json')
+    let shoesJSON = await response.json();
     let shoeDiv = document.getElementById("shoes-section");
    
-    
+    //loop through the list of shoes from the json file
+    for(i in shoesJSON){
+        let shoe = shoesJSON[i];
+        shoeDiv.append(getShoeItem(shoe));
+    }
 }
 
+function getShoeItem(shoe){
+    let shoeSection = document.createElement("section");
+    let h3Elem = document.createElement("h3");
+    h3Elem.innerText = shoe.name;
+    shoeSection.append(h3Elem);
+
+    let ulElem = document.createElement("ul");
+    shoeSection.append(ulElem);
+
+    let liPrice = document.createElement("li");
+    liPrice.innerText = `Price: ${shoe.price}`;
+    ulElem.append(liPrice);
+
+    let liTitle = document.createElement("li");
+    liTitle.innerText = `Title: ${shoe.title}`;
+    ulElem.append(liTitle);
+
+    let liReviews = document.createElement("li");
+    liReviews.innerText = `Reviews: ${shoe.reviews.join(', ')}`;
+    ulElem.append(liReviews);
+
+    return shoeSection;
+}
 
 window.onload = function(){
     this.showShoes();
