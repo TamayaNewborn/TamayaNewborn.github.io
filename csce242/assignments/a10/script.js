fd27e1b6e9a8c5e5138625063f2102064214832f
Tamaya Newborn <tamayanewborn@Tamayas-MacBook-Air.local>
2020-03-28 20:44:14 -0400

    final product

--- csce242/assignments/a10/script.js
+++ csce242/assignments/a10/script.js
@@ -13,47 +13,47 @@ async function showMovies(){
         moviesSection.classList.add("movies");
         let moviesImgSection = document.createElement("section");
 
+        //movie image
+        let moviesImg = document.createElement("IMG");
+         moviesImg.classList.add("moviesImg");
+         moviesImg.src = ` https://portiaportia.github.io/csce242/json/images ${movies.img}`;
+         moviesImgSection.append(moviesImg);
+         moviesSection.append(moviesImgSection);
+    
         //let moviesSection = document.createElement("section");
         let h3Elem = document.createElement("h3");
         h3Elem.innerText = movies.title;
         let h2Elem = document.createElement("h2");
-        h2Elem.innerText = ` ${movies.title}`;
+        h2Elem.innerHTML = `<center> ${movies.title}</center>`;
         moviesSection.append(h2Elem);
 
         let ulElem = document.createElement("ul");
-        moviesSection.append(ulElem);
-
-         //movie image
-         let moviesImg = document.createElement("IMG");
-         moviesImg.classList.add("moviesImg");
-         moviesImg.src = `images/${movies.title}.jpg`;
-         moviesImgSection.append(moviesImg);
-         moviesSection.append(moviesImgSection);
-    
+        moviesSection.append(ulElem);  
+         
         //director
-        let liDirector = document.createElement("li");
-        liDirector.innerHTML = `<b> Director: </b> ${movies.director}`;
-        ulElem.append(liDirector);
+        let pDirector = document.createElement("p");
+        pDirector.innerHTML = `<b> Director: </b> ${movies.director}`;
+        ulElem.append(pDirector);
 
         //actors
-        let liActors = document.createElement("li");
-        liActors.innerHTML = `<b> Actors:</b> ${movies.actors.join(', ')}`;
-        ulElem.append(liActors);
+        let pActors = document.createElement("p");
+        pActors.innerHTML = `<b> Actors:</b> ${movies.actors.join(', ')}`;
+        ulElem.append(pActors);
 
         //year
-        let liYear = document.createElement("li");
-        liYear.innerHTML = `<b> Year:</b> ${movies.year}`;
-        ulElem.append(liYear);
+        let pYear = document.createElement("p");
+        pYear.innerHTML = `<b> Year:</b> ${movies.year}`;
+        ulElem.append(pYear);
 
         //genres
-        let liGenres = document.createElement("li");
-        liGenres.innerHTML = `<b> Genres:</b> ${movies.genres.join(', ')}`;
-        ulElem.append(liGenres);
+        let pGenres = document.createElement("p");
+        pGenres.innerHTML = `<b> Genres:</b> ${movies.genres.join(', ')}`;
+        ulElem.append(pGenres);
 
         //description 
-        let liDescription = document.createElement("li");
-        liDescription.innerHTML = `<b> Description:</b> ${movies.description}`;
-        ulElem.append(liDescription);
+        let pDescription = document.createElement("p");
+        pDescription.innerHTML = `<b> Description:</b> ${movies.description}`;
+        ulElem.append(pDescription);
 
 
         return moviesSection;
